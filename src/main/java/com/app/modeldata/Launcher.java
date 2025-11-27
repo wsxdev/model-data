d058474d74d66aaaa6c7ea007dc1eadae37ca41d
wsxdev <williamsxdev@gmail.com>
2025-11-27 12:05:52 -0500

    Add: creación del proyecto

---
+++ src/main/java/com/app/modeldata/Launcher.java
@@ -0,0 +1,9 @@
+package com.app.modeldata;
+
+import javafx.application.Application;
+
+public class Launcher {
+    public static void main(String[] args) {
+        Application.launch(HelloApplication.class, args);
+    }
+}
