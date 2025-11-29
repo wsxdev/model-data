47fe0a8922ce14c88e339f1cb4423f31f5a8dfa4
licetdev <marialiset13@gmail.com>
2025-11-28 19:36:42 -0500

    Add: class ModelDataApp

---
+++ src/main/java/com/app/modeldata/ModelDataApp.java
@@ -0,0 +1,16 @@
+package com.app.modeldata;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class ModelDataApp extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
