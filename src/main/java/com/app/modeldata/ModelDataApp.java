5ab3784920696269f3dcc0c7c67e15eed1d590ef
wsxdev <williamsxdev@gmail.com>
2025-11-30 20:18:30 -0500

    Feat: Icono para la app al ejecutar

--- src/main/java/com/app/modeldata/ModelDataApp.java
+++ src/main/java/com/app/modeldata/ModelDataApp.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -13,6 +14,11 @@ public class ModelDataApp extends Application {
     public void start(Stage stage) throws IOException {
         // PARA CARGAR EL LOGIN ANTES DE LA MAIN VIEW
         FXMLLoader loginLoader = new FXMLLoader(ModelDataApp.class.getResource("/com/app/modeldata/fxml/login/login-vista.fxml"));
+
+        // AÑADIR ICONO AL STAGE
+        stage.getIcons().add(new Image(
+                getClass().getResourceAsStream("/com/app/modeldata/images/logos/ModelDataLogoConBG.png")
+        ));
 
         Parent root = loginLoader.load();
         Scene sceneLogin = new Scene(root);
